c7100111c92e1dc4d6a8ac1c5bfef32d372b9088
CarpenterTic <bertrand.charpentier.batir@gmail.com>
2015-09-21 18:13:14 +0200

    essai 01

---
+++ exercises/014/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+import sys
+if(len(sys.argv[1:]))>=1:
+    print(sys.argv)
+else :
+    print("usage: python3 solution.py PARAM")
