047c6105fae07d7d68d7ba3b269e84a7a9fc7fac
CarpenterTic <bertrand.charpentier.batir@gmail.com>
2015-09-22 16:13:14 +0200

    essai 1

---
+++ exercises/070/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 14:12:53 2015
+
+@author: Ars
+"""
+import string
+alphabet1 = string.ascii_lowercase
+for caract1 in alphabet:
+    for caract2 in alphabet:
+        if caract1 != caract2:
+            print(caract1 + caract2)
