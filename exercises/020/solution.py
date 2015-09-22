bc552b633a44d1ead7082caeee5e4d4af0b40876
CarpenterTic <bertrand.charpentier.batir@gmail.com>
2015-09-22 09:58:00 +0200

    essai1

---
+++ exercises/020/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 19:07:17 2015
+
+@author: Ars
+"""
+
+import sys
+if len(sys.argv) == 3:
+    print(int(sys.argv[1]) - int(sys.argv[2]))
+else:
+    print("usage: python3 solution.py OP1 OP2")
