af7d20e8b65389f9f96b9b18805734e15cefbedb
CarpenterTic <bertrand.charpentier.batir@gmail.com>
2015-09-22 11:03:39 +0200

    essai1

---
+++ exercises/025/solution.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 10:02:23 2015
+
+@author: Ars
+"""
+import datetime, time
+print("today is", date.today(), " and it is", time.strftime('%H:%M:%S'))
