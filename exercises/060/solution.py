22198df8b8601f16c3dce315317eec65e1f7f2ea
CarpenterTic <bertrand.charpentier.batir@gmail.com>
2015-09-22 15:42:06 +0200

    essai 1

---
+++ exercises/060/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 14:12:53 2015
+
+@author: Ars
+"""
+import string
+alphabet = string.ascii_lowercase
+for caract1 in alphabet:
+        for caract2 in alphabet:
+            print(caract1, caract2)
