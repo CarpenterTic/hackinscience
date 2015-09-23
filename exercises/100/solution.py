2160e5e2e959cd35acda1ab695d8a770a24638ef
CarpenterTic <bertrand.charpentier.batir@gmail.com>
2015-09-23 18:21:50 +0200

    essai1

---
+++ exercises/100/solution.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 14:12:53 2015
+
+@author: Ars
+"""
+
+station = {
+ 'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
+ 'number': 31705,
+ 'latitude': 48.8645278209514,
+ 'name': 'CHAMPEAUX (BAGNOLET)',
+ 'longitude': 2.416170724425901
+}
+
+for clef, valeur in station.items():
+    print(clef, valeur)
