002dd8cc318634bc2b8b5687f7af3fd52480d407
CarpenterTic <bertrand.charpentier.batir@gmail.com>
2015-09-23 18:27:53 +0200

    essai2

--- exercises/100/solution.py
+++ exercises/100/solution.py
@@ -6,11 +6,11 @@ Created on Tue Sep 22 14:12:53 2015
 """
 
 station = {
- 'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
- 'number': 31705,
- 'latitude': 48.8645278209514,
- 'name': 'CHAMPEAUX (BAGNOLET)',
- 'longitude': 2.416170724425901
+    'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
+    'number': 31705,
+    'latitude': 48.8645278209514,
+    'name': 'CHAMPEAUX (BAGNOLET)',
+    'longitude': 2.416170724425901
 }
 
 for clef, valeur in station.items():
