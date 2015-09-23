eb8eadeb4c97aa07f20243d1e1bd2a047fe8b31e
CarpenterTic <bertrand.charpentier.batir@gmail.com>
2015-09-23 13:44:01 +0200

    essai1

---
+++ exercises/090/solution.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 14:12:53 2015
+
+@author: Ars
+"""
+
+L1 = ['Spring', 'Summer', 'Fall', 'Winter']
+for i, item in enumerate(L1):
+    print(i, item)
