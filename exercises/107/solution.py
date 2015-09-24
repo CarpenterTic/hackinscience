6fcfd04baaf19508faa5438e5595148e86343400
CarpenterTic <bertrand.charpentier.batir@gmail.com>
2015-09-24 16:45:52 +0200

    essai2

--- exercises/107/solution.py
+++ exercises/107/solution.py
@@ -7,15 +7,15 @@ Created on Tue Sep 22 14:12:53 2015
 
 
 def select_student(my_class, mark):
-    Accepted = []
-    Refused = []
+    accepted = []
+    refused = []
     for student in my_class:
         if mark >= student[1]:
             Accepted.append(student)
         if mark < student[1]:
             Refused.append(student)
 
-    sorted_asc = sorted(Accepted, key=lambda score: score[1])
-    sorted_desc = sorted(Refused, key=lambda score: score[1], reverse=True)
+    sorted_asc = sorted(accepted, key=lambda score: score[1])
+    sorted_desc = sorted(refused, key=lambda score: score[1], reverse=True)
     return {"Accepted": sorted_desc,
             "Refused": sorted_asc}
